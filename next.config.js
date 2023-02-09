66cfd6b323a43c844ae322da78aaffeb75c7514b
Genius <genius21513@gmail.com>
2023-02-09 11:17:01 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   reactStrictMode: true,
   distDir: './build',
   images: {
     domains: ['res.cloudinary.com'],
   },
+  assetPrefix: isProd? "/metakrew.com/" : "",
+  basePath: isProd? "/metakrew.com/" : ""
 }
